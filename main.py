c8c1a3c464e03de798ae9f5c2978fc505b542ba7
Odufuwa Adebola <adeboladuf@gmail.com>
2025-04-08 16:41:59 +0100

    Update error page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from starlette.middleware.sessions import SessionMiddleware
 from fastapi.staticfiles import StaticFiles
 from core.config import settings
